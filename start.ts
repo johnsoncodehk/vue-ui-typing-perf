e3b4bd489dbdcf7d3b4e693da9feee0a455ec5a3
Johnson Chu <johnsoncodehk@gmail.com>
2023-07-30 10:32:03 +0800

    first commit

---
+++ start.ts
@@ -0,0 +1,273 @@
+import { setup } from './utils';
+import * as path from 'path';
+import { execaCommand } from 'execa';
+import * as fs from 'fs';
+import * as assert from 'assert';
+
+const sample = 1; // less for faster, bigger for smooth numbers
+
+(async () => {
+
+    const context = await setup(path.join(__dirname, 'servers'), {
+        'v1.8.8': { npm: '@vue/language-server@1.8.8', bin: './bin/vue-language-server.js' },
+    });
+    const libraries: {
+        name: string;
+        deps: string[];
+        version: string;
+    }[] = [
+            { name: 'vanilla', deps: ['vue'], version: 'latest' },
+            { name: 'ant-design-vue', deps: ['ant-design-vue'], version: 'latest' },
+            { name: 'bootstrap-vue-next', deps: ['bootstrap-vue-next'], version: 'latest' },
+            { name: 'element-plus', deps: ['element-plus'], version: 'latest' },
+            { name: 'naive-ui', deps: ['naive-ui'], version: 'latest' },
+            {
+                name: 'oku-ui', deps: [
+                    '@oku-ui/arrow',
+                    '@oku-ui/aspect-ratio',
+                    '@oku-ui/avatar',
+                    '@oku-ui/checkbox',
+                    '@oku-ui/collapsible',
+                    '@oku-ui/label',
+                    '@oku-ui/popper',
+                    '@oku-ui/presence',
+                    '@oku-ui/progress',
+                    '@oku-ui/separator',
+                    // '@oku-ui/switch',
+                    '@oku-ui/toggle',
+                    // '@oku-ui/visually-hidden',
+                ], version: 'latest'
+            },
+            { name: 'quasar', deps: ['quasar'], version: 'latest' },
+            { name: 'radix-vue', deps: ['radix-vue'], version: 'latest' },
+            { name: 'vuetify', deps: ['vuetify'], version: 'latest' },
+
+            // NO TS
+            // { name: 'primevue', deps: ['primevue'], version: 'latest' },
+            // { name: 'keen-ui', deps: ['keen-ui'], version: 'latest' },
+        ];
+
+    const pathCompletionTsvRows: (string | number)[][] = [['']];
+    const autoImportTsvRows: (string | number)[][] = [['']];
+    const setupContextTsvRows: (string | number)[][] = [['']];
+
+    const tempPath = path.join(__dirname, 'temp');
+    if (!fs.existsSync(tempPath)) {
+        fs.mkdirSync(tempPath);
+    }
+
+    for (const library of libraries) {
+
+        console.log(library.name + '...');
+
+        pathCompletionTsvRows[0].push(library.name);
+        autoImportTsvRows[0].push(library.name);
+        setupContextTsvRows[0].push(library.name);
+
+        /**
+         * Setup
+         */
+        const folder = path.join(tempPath, library.name);
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder);
+            const dependencies = { vue: 'latest' };
+            for (const dep of library.deps) {
+                dependencies[dep] = library.version;
+            }
+            fs.writeFileSync(path.join(folder, 'package.json'), JSON.stringify({ dependencies }));
+            fs.writeFileSync(path.join(folder, 'tsconfig.json'), JSON.stringify({}));
+            await execaCommand(`pnpm i`, { cwd: folder }).pipeStdout?.(process.stdout);
+        }
+
+        /**
+         * Path Completion
+         */
+        fs.writeFileSync(path.join(folder, 'App.vue'), [
+            `<script setup lang="ts">`,
+            ...library.deps.map(dep => `import {} from '${dep}';`),
+            `import {} from './';`,
+            `</script>`,
+        ].join('\n'));
+
+        const pathCompletionResult = await context.start({
+            rootPath: folder,
+            repeatTimes: sample,
+        }, async (connection, timekeeper, helpers) => {
+
+            connection.onRequest('client/registerCapability', () => { /* ignore */ });
+            connection.onRequest('workspace/configuration', () => { /* ignore */ });
+
+            await helpers.initialize({ typescript: { tsdk: path.join(__dirname, 'node_modules', 'typescript', 'lib') } });
+
+            let document = await helpers.openDocument('./App.vue', 'vue');
+
+            await helpers.requestCompletion(document, document.positionAt(document.getText().lastIndexOf(`';`))); // ignore first time
+
+            for (let i = 0; i < 50; i++) {
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(
+                        document,
+                        document.positionAt(0),
+                        document.positionAt(document.getText().length),
+                        document.getText().includes(`'./';`)
+                            ? document.getText().replace(`'./';`, `'';`)
+                            : document.getText().replace(`'';`, `'./';`)
+                    );
+                    await helpers.requestCompletion(document, document.positionAt(document.getText().lastIndexOf(`';`)));
+                });
+            }
+        });
+
+        /**
+         * Global Completion
+         */
+        fs.writeFileSync(path.join(folder, 'App.vue'), [
+            `<script lang="ts">`,
+            ...library.deps.map(dep => `import {} from '${dep}';`),
+            `/* __complete__ */`,
+            `</script>`,
+        ].join('\n'));
+
+        const autoImportResult = await context.start({
+            rootPath: folder,
+            repeatTimes: sample,
+        }, async (connection, timekeeper, helpers) => {
+
+            connection.onRequest('client/registerCapability', () => { /* ignore */ });
+            connection.onRequest('workspace/configuration', () => { /* ignore */ });
+
+            await helpers.initialize({ typescript: { tsdk: path.join(__dirname, 'node_modules', 'typescript', 'lib') } });
+
+            let document = await helpers.openDocument('./App.vue', 'vue');
+
+            const getActionPos = () => document.positionAt(document.getText().lastIndexOf(`/* __complete__ */`));
+
+            await helpers.requestCompletion(document, getActionPos()); // ignore first time
+
+            for (let i = 0; i < 10; i++) {
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'w');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'watch'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'a');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'watch'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 't');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'watch'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'c');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'watch'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'h');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'watch'), true);
+                });
+
+                // delete added text
+                document = await helpers.editDocument(document, { line: getActionPos().line, character: getActionPos().character - 5 }, getActionPos(), '');
+            }
+        });
+
+        /**
+         * setup() Completion
+         */
+        fs.writeFileSync(path.join(folder, 'App.vue'), [
+            `<script lang="ts">`,
+            ...library.deps.map(dep => `import {} from '${dep}';`),
+            `export default { setup(props) {`,
+            `/* __complete__ */`,
+            `} }`,
+            `</script>`,
+        ].join('\n'));
+
+        const setupContextResult = await context.start({
+            rootPath: folder,
+            repeatTimes: sample,
+        }, async (connection, timekeeper, helpers) => {
+
+            connection.onRequest('client/registerCapability', () => { /* ignore */ });
+            connection.onRequest('workspace/configuration', () => { /* ignore */ });
+
+            await helpers.initialize({ typescript: { tsdk: path.join(__dirname, 'node_modules', 'typescript', 'lib') } });
+
+            let document = await helpers.openDocument('./App.vue', 'vue');
+
+            const getActionPos = () => document.positionAt(document.getText().lastIndexOf(`/* __complete__ */`));
+
+            await helpers.requestCompletion(document, getActionPos()); // ignore first time
+
+            for (let i = 0; i < 10; i++) {
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'p');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'props'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'r');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'props'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'o');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'props'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 'p');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'props'), true);
+                });
+
+                await timekeeper(async () => {
+                    document = await helpers.editDocument(document, getActionPos(), getActionPos(), 's');
+                    assert.equal((await helpers.requestCompletion(document, getActionPos()) as any).items.some(item => item.label === 'props'), true);
+                });
+
+                // delete added text
+                document = await helpers.editDocument(document, { line: getActionPos().line, character: getActionPos().character - 5 }, getActionPos(), '');
+            }
+        });
+
+        const times_1 = Object.values(pathCompletionResult)[0];
+        for (let j = 0; j < times_1.length; j++) {
+            if (pathCompletionTsvRows.length <= j + 1) {
+                pathCompletionTsvRows.push([j + 1]);
+            }
+            pathCompletionTsvRows[j + 1].push(times_1[j])
+        }
+
+        const times_2 = Object.values(autoImportResult)[0];
+        for (let j = 0; j < times_2.length; j++) {
+            if (autoImportTsvRows.length <= j + 1) {
+                autoImportTsvRows.push([j + 1]);
+            }
+            autoImportTsvRows[j + 1].push(times_2[j])
+        }
+
+        const times_3 = Object.values(setupContextResult)[0];
+        for (let j = 0; j < times_3.length; j++) {
+            if (setupContextTsvRows.length <= j + 1) {
+                setupContextTsvRows.push([j + 1]);
+            }
+            setupContextTsvRows[j + 1].push(times_3[j])
+        }
+    }
+
+    saveTsv(path.join(tempPath, 'path_completion.tsv'), pathCompletionTsvRows.map(arr => arr.join('\t')).join('\n'));
+    saveTsv(path.join(tempPath, 'global_completion.tsv'), autoImportTsvRows.map(arr => arr.join('\t')).join('\n'));
+    saveTsv(path.join(tempPath, 'setup_function_completion.tsv'), setupContextTsvRows.map(arr => arr.join('\t')).join('\n'));
+})();
+
+function saveTsv(path: string, data: string) {
+    console.log(data);
+    console.log(path);
+    fs.writeFileSync(path, data);
+}
---
+++ utils.ts
@@ -0,0 +1,166 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { execaCommand } from 'execa';
+import * as cp from 'child_process';
+import * as protocol from 'vscode-languageserver-protocol/node';
+import { URI } from 'vscode-uri';
+
+export async function setup(serversDir: string, pkgs: Record<string,
+    { module: string }
+    | { npm: string, bin: string }
+>) {
+
+    const packageJson = { dependencies: {} };
+
+    for (const [serverName, mod] of Object.entries(pkgs)) {
+        if ('npm' in mod) {
+            packageJson.dependencies[serverName] = 'npm:' + mod.npm;
+        }
+    }
+
+    if (!fs.existsSync(serversDir)) {
+        fs.mkdirSync(serversDir);
+    }
+    fs.writeFileSync(path.join(serversDir, 'package.json'), JSON.stringify(packageJson, undefined, 2));
+
+    await execaCommand(`pnpm i`, { cwd: serversDir }).pipeStdout?.(process.stdout);
+
+    return { start };
+
+    async function start(
+        options: {
+            rootPath: string;
+            repeatTimes: number;
+            tsvPath?: string;
+        },
+        fn: (
+            connection: protocol.ProtocolConnection,
+            timekeeper: (fn: () => void, eventName?: string) => Promise<void>,
+            helpers: ReturnType<typeof createHelpers>
+        ) => void
+    ) {
+
+        const result: Record<string, number[]> = {};
+        const eventNames: string[] = [];
+
+        for (let i = 0; i < options.repeatTimes; i++) {
+            for (const serverName in pkgs) {
+                const mod = pkgs[serverName];
+                const serverModule = 'npm' in mod
+                    ? path.resolve(path.join(serversDir, 'node_modules', serverName), mod.bin)
+                    : path.resolve(serversDir, mod.module);
+                // console.log(`${serverName}...`, serverModule);
+                const childProcess = cp.fork(
+                    serverModule,
+                    [
+                        '--node-ipc',
+                        `--clientProcessId=${process.pid.toString()}`,
+                    ],
+                    {
+                        execArgv: ['--nolazy'],
+                        env: process.env,
+                        cwd: options.rootPath,
+                        silent: true,
+                    });
+                if (!childProcess.stdout || !childProcess.stdin || !childProcess.stderr) {
+                    throw new Error('Process created without stdio streams');
+                }
+                const times: number[] = [];
+                const connection = protocol.createProtocolConnection(
+                    new protocol.IPCMessageReader(childProcess),
+                    new protocol.IPCMessageWriter(childProcess)
+                );
+                connection.listen();
+                eventNames.length = 0;
+
+                await fn(
+                    connection,
+                    async (fn, eventName) => {
+                        const start = Date.now();
+                        try {
+                            await fn();
+                        }
+                        catch (e) {
+                            console.error(e);
+                            throw e;
+                        }
+                        const duration = Date.now() - start;
+                        times.push(duration);
+                        eventNames.push(eventName || (eventNames.length + 1).toString());
+                    },
+                    createHelpers(options.rootPath, connection)
+                );
+
+                childProcess.kill();
+                if (result[serverName]) {
+                    result[serverName] = result[serverName].map((time, i) => time + times[i]);
+                }
+                else {
+                    result[serverName] = times;
+                }
+            }
+        }
+
+        if (options.repeatTimes > 1) {
+            for (const times of Object.values(result)) {
+                for (let i = 0; i < times.length; i++) {
+                    times[i] /= options.repeatTimes;
+                }
+            }
+        }
+
+        const serverNames = Object.keys(pkgs);
+        let tsv = ['', ...serverNames].join('\t') + '\n';
+        for (let i = 0; i < eventNames.length; i++) {
+            tsv += [eventNames[i], ...serverNames.map(serverName => result[serverName][i])].join('\t') + '\n';
+        }
+        if (options.tsvPath) {
+            fs.writeFileSync(options.tsvPath, tsv);
+            console.log('Saved result to', options.tsvPath);
+        }
+        return result;
+    }
+
+    function createHelpers(dir: string, connection: protocol.ProtocolConnection) {
+        return {
+            async initialize(initializationOptions: any) {
+                const result = await connection.sendRequest('initialize', {
+                    rootPath: dir,
+                    capabilities: {},
+                    initializationOptions: initializationOptions,
+                });
+                await connection.sendNotification('initialized');
+                return result;
+            },
+            async openDocument(filePath: string, languageId: string) {
+                const fileName = path.resolve(dir, filePath);
+                const uri = URI.file(fileName).toString();
+                const item = protocol.TextDocumentItem.create(uri, languageId, 0, fs.readFileSync(fileName, 'utf-8'));
+                await connection.sendNotification(protocol.DidOpenTextDocumentNotification.type, {
+                    textDocument: item,
+                });
+                return protocol.TextDocument.create(uri, languageId, 0, item.text);
+            },
+            async editDocument(document: protocol.TextDocument, start: protocol.Position, end: protocol.Position, text: string) {
+                const newText = protocol.TextDocument.applyEdits(document, [protocol.TextEdit.replace({ start, end }, text)]);
+                const newDocument = protocol.TextDocument.create(document.uri, document.languageId, document.version + 1, newText);
+                await connection.sendNotification(protocol.DidChangeTextDocumentNotification.type, {
+                    textDocument: {
+                        uri: newDocument.uri,
+                        version: newDocument.version,
+                    },
+                    contentChanges: [{ range: { start, end }, text }],
+                });
+                return newDocument;
+            },
+            async requestCompletion(document: protocol.TextDocument, start: protocol.Position) {
+                return await connection.sendRequest(protocol.CompletionRequest.type, {
+                    textDocument: {
+                        uri: document.uri,
+                    },
+                    position: start,
+                });
+            },
+        }
+    }
+}
